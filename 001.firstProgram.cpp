0f6d704dfd20fd05fd321258801fa0f590a1fe7d
Jyoti Pandey <jyotipandey9813@gmail.com>
2025-08-16 00:09:15 +0530

    (CPP-001): First CPP program.

---
+++ 001.firstProgram.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "My name is Jyoti" << std::endl;
+    std::cout << "I will learn cpp by 31/08/2024" << std::endl;
+    std::cout << "Today is 19/09/2024, and I started cpp programming" << std::endl;
+    return 0;
+} 
